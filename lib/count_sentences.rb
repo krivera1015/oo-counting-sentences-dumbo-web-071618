3b0672ac618f3affad94eb9090512843da6ac2d4
Kevin RRivera <krivera1015@gmail.com>
2018-07-19 20:58:23 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,37 @@ require 'pry'
 
 class String
 
+  attr_accessor :sentence
+
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+    # binding.pry
 
+    new_var = self.gsub(/[^A-Za-z0-9\s]/i, '').split(" ")
+    # binding.pry
+    return new_var.count
   end
 end
